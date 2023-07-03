8c374ed49c3ed9d64583639449bfd184573ecd24
Andreas Poehlmann <andreas@poehlmann.io>
2023-07-03 22:30:46 +0200

    upath: lazy import UPath from upath.core

--- upath/__init__.py
+++ upath/__init__.py
@@ -1,9 +1,22 @@
 """Pathlib API extended to use fsspec backends."""
-from upath.core import UPath
+from typing import TYPE_CHECKING
 
 try:
     from upath._version import __version__
 except ImportError:
     __version__ = "not-installed"
 
+
 __all__ = ["UPath"]
+
+if TYPE_CHECKING:
+    from upath.core import UPath
+
+
+def __getattr__(name):
+    if name == "UPath":
+        from upath.core import UPath
+
+        return UPath
+    else:
+        raise AttributeError(name)
