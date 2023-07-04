3b120ecfda6742199116f4c220d2359ff265d4b6
Andreas Poehlmann <andreas@poehlmann.io>
2023-07-04 19:43:30 +0200

    upath.core: re-add UPath.fs and UPath.path

--- upath/core.py
+++ upath/core.py
@@ -3,6 +3,11 @@ from __future__ import annotations
 import os
 import sys
 
+from fsspec import get_filesystem_class
+from fsspec.core import split_protocol
+from fsspec.core import strip_protocol
+from fsspec.spec import AbstractFileSystem
+
 from upath._flavour import fsspecpath
 
 if sys.version_info >= (3, 12):
@@ -29,12 +34,32 @@ class PureFSSpecPath(PurePath):
 
 
 class UPath(Path):
-    __slots__ = ()
+    __slots__ = ("_protocol", "_storage_options", "_cached_fs")
 
     def __new__(cls, *args, **kwargs):
         if cls is UPath:
-            cls = WindowsUPath if os.name == "nt" else PosixUPath
+            if (
+                (args and split_protocol(args[0])[0] is not None)
+                or kwargs.get("protocol")
+                or kwargs.get("fs")
+            ):
+                cls = FSSpecUPath
+            else:
+                cls = WindowsUPath if os.name == "nt" else PosixUPath
         return object.__new__(cls)
+
+    @property
+    def fs(self) -> AbstractFileSystem:
+        try:
+            return self._cached_fs
+        except AttributeError:
+            fs_cls = get_filesystem_class(self._protocol)
+            self._cached_fs = fs = fs_cls(**self._storage_options)
+            return fs
+
+    @property
+    def path(self) -> str:
+        return strip_protocol(self)
 
 
 class PosixUPath(UPath, PosixPath):
